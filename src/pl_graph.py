6883d21802a3007cd3572a67760be0325a032a3f
Ruizheng Xu <48921807+ruizhengxu@users.noreply.github.com>
2021-12-01 17:20:45 +0100

    PL finish

--- src/pl_graph.py
+++ src/pl_graph.py
@@ -16,15 +16,12 @@ VERTICES = int(lines[0])
 EDGES = int(lines[1])
 
 origin = 'a'
-destination = 'l'
+destination = 'k'
+sdate = 3
+edate = 20
 
 links = gp.tuplelist()
-nodes = []
 cost = {}
-date = {}
-
-for n in lines[2:2+VERTICES]:
-    nodes.append(n)
     
 for l in lines[2+VERTICES:len(lines)]:
     l = l.replace('(', '').replace(')', '').split(',')
@@ -32,13 +29,11 @@ for l in lines[2+VERTICES:len(lines)]:
     to_node = l[1]
     depart_date = int(l[2])
     travel_cost = int(l[3])
-    links.append((from_node, to_node, int(depart_date)))
-    date[(from_node, to_node, int(depart_date))] = int(depart_date)
-    cost[(from_node, to_node, int(depart_date))] = int(travel_cost)
-
-# print("links : ", links)
-# print("date :", date)
-# print("cost :", cost)
+    if (from_node == origin and int(depart_date) >= sdate)\
+        or (to_node == destination and int(depart_date) <= edate)\
+        or (from_node != origin and to_node != destination):
+        links.append((from_node, to_node, int(depart_date)))
+        cost[(from_node, to_node, int(depart_date))] = int(travel_cost)
 
 # Create model
 m = gp.Model()
@@ -49,29 +44,13 @@ m.update()
 
 # Add constraints
 # Only one arc which leave from the node containing origin can be chosen
-# m.addConstr(-sum(x[i, j, k] for i, j, k in links.select(origin, '*', '*')) <= -1)
-# # Only one arc which arrive in the node containing destination can be chosen
-# m.addConstr(-sum(x[i, j, k] for i, j, k in links.select('*', destination, '*')) <= -1)
-# # One arc between every nodes between node containing origin and destination
-# for node in nodes:
-#     if node != origin and node != destination:
-#         m.addConstr(
-#             sum(x[i, j, k] for i, j, k in links.select(node, '*', '*'))\
-#             - sum(x[i_, j_, k_] for i_, j_, k_ in links.select('*', node, '*'))\
-#             <= 2)
-# date+1 of the previous node should be lower than the current date
-for node in nodes:
-    for i, j, k in links.select(node, '*', '*'):
-        for i_, j_, k_ in links.select(j, '*', '*'):
-            m.addConstr( x[i, j, k]*date[i, j, k]\
-            - x[i_, j_, k_]*date[i_,j_,k_]\
-                <= 5) # à changer, 5 vient de nul part mais marche sur a -> l, pas sur tous les chemins
-
-for node in nodes:
-    m.addConstr( sum(x[i, j, k] for i, j, k in links.select(node, '*', '*'))\
-                - sum(x[j, i, k] for j, i, k in links.select('*', node, '*')) == 
-                (1 if node == origin else -1 if node == destination else 0 ),'node%s_' % node )
-
+m.addConstr(sum(x[i, j, k] for i, j, k in links.select(origin, '*', '*')) >= 1)
+# Only one arc which arrive in the node containing destination can be chosen
+m.addConstr(sum(x[i, j, k] for i, j, k in links.select('*', destination, '*')) >= 1)
+# If an arc is taken, so its precessor should also be taken
+for i, j, k in links:
+    if i != origin:
+        m.addConstr(x[i, j, k] <= sum(x[i_, j_, k_] for i_, j_, k_ in links.select('*', i, '*') if k >= k_+1 ))
 
 # Solve problem
 m.optimize()
